b75d39b722000420d190a99b26eefadc64ae48ab
Mythsoul <Mythsoul2023@gmail.com>
2025-01-02 17:38:53 +0545

    check input

--- fe/src/components/Typearea.jsx
+++ fe/src/components/Typearea.jsx
@@ -7,7 +7,10 @@ function Typearea() {
   const [language, setLanguage] = useState('english');
   const [pressedKey, setPressedKey] = useState('');
   const [istextloading, setIstextloading] = useState(false);
-
+  const [isTyping, setIsTyping] = useState(false);
+  const [isTypingCorrect, setIsTypingCorrect] = useState(false);
+  const [isTypinginCorrect, setIsTypinginCorrect] = useState(false);
+  
   useEffect(() => {
     fetch(`https://api.allorigins.win/raw?url=https://monkeytype.com/languages/${language}.json`)
       .then(response => response.json())
@@ -46,6 +49,16 @@ switch(key){
         setPressedKey('');
     }, 0.001);
   }
+  const handleinput = (e) => {
+    const value = e.target.value;
+    const targetText = words.join(' ');
+    const correct = value.split('').every((char, index) => {
+      return char === targetText[index];
+    });
+  
+    setIsTypingCorrect(correct && value.length > 0);
+    setIsTypinginCorrect(!correct && value.length > 0);
+  }
   async function getrandomWords(){ 
     try{ 
         if (Allwords.length === 0) return;
@@ -59,14 +72,23 @@ switch(key){
   }
 return (
 
-        <div className='flex justify-center items-center h-screen'>
+        <div className='flex justify-center  h-screen'>
             {istextloading && <p className="text-3xl font-mono p-4 m-10">Loading...</p>}
-            <div className="words flex flex-wrap justify-center m-[100px]">
-                {words.map((word, index) => { 
-                    return <div key={index} className="word text-black m-1   p-2 borderrounded">{word}</div>
-                })}
+            <div className='words-box '>
+              <div className="words m-32 border-black border-2 rounded p-4 text-red-700 font-mono text-xl" >
+            {words.join(' ')}
             </div>
+         
+        <div className='typearea'>
+<textarea type="text" placeholder='Enter the words above ' className='typearea border-black border-2 rounded text-xl ml-[300px]'  onChange={handleinput}/>
         </div>
+        <div className='text-center'>
+            {isTypingCorrect && <p className='text-3xl font-mono p-4 m-10'>Correct!</p>}
+            {isTypinginCorrect && <p className='text-3xl font-mono p-4 m-10'>Incorrect!</p>}  
+       </div>
+        </div>
+        </div>
+        
 )
 }
 
