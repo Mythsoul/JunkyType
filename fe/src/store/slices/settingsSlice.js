0b72ab4f006c2e1b1c20b7a6894048df4c39c623
Mythsoul <Mythsoul2023@gmail.com>
2025-01-03 19:34:30 +0545

    redux setup

---
+++ fe/src/store/slices/settingsSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  language: 'english',
+  theme: 'dark',
+  wordCount: 20,
+};
+
+const settingsSlice = createSlice({
+  name: 'settings',
+  initialState,
+  reducers: {
+    setLanguage: (state, action) => {
+      state.language = action.payload;
+    },
+    setTheme: (state, action) => {
+      state.theme = action.payload;
+    },
+    setWordCount: (state, action) => {
+      state.wordCount = action.payload;
+    },
+  },
+});
+
+export const { setLanguage, setTheme, setWordCount } = settingsSlice.actions;
+export default settingsSlice.reducer;
---
+++ fe/src/store/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import settingsReducer from './slices/settingsSlice';
+
+const store = configureStore({
+  reducer: {
+    settings: settingsReducer,
+  },
+});
+
+export default store;
+
