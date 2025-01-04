988fff4bbda4da547564e451b8b47ab75bc9c50a
Mythsoul <mythsoul2023@gmail.com>
2025-01-04 09:30:00 +0545

    fix deployment issues

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,15 +1,23 @@
 import React from "react";
-import Navbar from "./components/navbar";
-import Home from "./components/home";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar';
 import Typearea from "./components/Typearea";
-function App(){ 
-return(
-    <>
- <Navbar/> 
- <Home /> 
- <Typearea /> 
- </>
-)
+import Leaderboard from "./components/Leaderboard";
+import Profile from "./components/Profile";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="min-h-screen bg-gray-800">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Typearea />} />
+          <Route path="/leaderboard" element={<Leaderboard />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App  
+export default App;
